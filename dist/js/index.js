daf03048fa41f935d9d9d829975a1b3f52e2163b
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-15 17:34:28 +0200

    Implemented random post functions

--- dist/js/index.js
+++ dist/js/index.js
@@ -8,6 +8,9 @@ const userWall = document.querySelector('.user-wall');
 const postAuthor = document.querySelectorAll('.post-author');
 const renderUserButton = document.querySelector('.render-new-user');
 const header = document.querySelector('.header');
+const postBodyOne = document.querySelector('.post-one');
+const postBodyTwo = document.querySelector('.post-two');
+console.log(postBodyOne, postBodyTwo);
 
 const generateRandomUser = () => {
 	fetch('https://randomuser.me/api/')
@@ -15,6 +18,17 @@ const generateRandomUser = () => {
 		.then((data) => {
 			const user = data.results[0];
 			renderUser(user);
+		});
+};
+
+const getPosts = () => {
+	fetch('js/posts.json')
+		.then((res) => res.json())
+		.then((data) => {
+			const randomPostOne = Math.floor(Math.random() * data.length);
+			const randomPostTwo = Math.floor(Math.random() * data.length);
+			postBodyOne.textContent = data[randomPostOne].body;
+			postBodyTwo.textContent = data[randomPostTwo].body;
 		});
 };
 
@@ -40,6 +54,8 @@ const displayUserPosts = (user) => {
 	postAuthor.forEach((title) => {
 		title.textContent = `${user.name.first} ${user.name.last}`;
 	});
+
+	getPosts();
 };
 
 const generateRandomBackgroundImage = () => {
